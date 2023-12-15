372acd47d97fccbfb8c014d7d2b9fcdc771cdf74
Samuil Dimitrov <sam.dimitrov@abv.bg>
2023-12-15 23:27:41 +0200

    Add project files.

---
+++ Connection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Phonebook
+{
+    class Connection
+    {
+        OleDbConnection connect;
+        OleDbCommand command;
+
+        private void ConnectionTo()
+        {
+            connect = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\LENOVO\Desktop\Phonebook.accdb");
+            command = connect.CreateCommand();
+        }
+
+        public Connection()
+        {
+            ConnectionTo();
+        }
+
+        public void Insert(Payment payment)
+        {
+            try
+            {
+                command.CommandText = $"Insert into Payment([Invoice],PaymentDate,[PhoneNumber],PhoneBill,PaymentStatus) values ('{payment.Invoice}','{payment.PaymentDate.Day}.{payment.PaymentDate.Month}.{payment.PaymentDate.Year}','{payment.PhoneNumber}',{payment.PhoneBill},{payment.PaymentStatus})";
+                command.CommandType = System.Data.CommandType.Text;
+                connect.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Incorrect data");
+            }
+            finally
+            {
+                if (connect != null)
+                {
+                    connect.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Phonebook
+{
+    public partial class Form1 : Form
+    {
+        Connection connection = new Connection();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: Change datetimepicker to be now writeable in
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.SelectedIndex = 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Payment payment = new Payment();
+            payment.Invoice = textBox1.Text;
+            payment.PaymentDate = dateTimePicker1.Value.Date;
+            payment.PhoneNumber = textBox2.Text;
+            payment.PhoneBill = Decimal.Parse(textBox3.Text);
+            // 0 for payed 1 for unpayed
+            payment.PaymentStatus = comboBox1.SelectedIndex == 0 ? true : false;
+            connection.Insert(payment);
+        }
+    }
+}
